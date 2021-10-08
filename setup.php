a128694db26eddbe33512c0fe3640202b42a901a
pixdoet <63037136+pixdoet@users.noreply.github.com>
2021-10-08 22:48:23 +0800

    Add files via upload

---
+++ setup.php
@@ -0,0 +1,11 @@
+<?php
+include("config.inc.php");
+
+$username = "";
+$password = "";
+
+$password_hash($password,PASSWORD_DEFAULT);
+$register = $conn->prepare("INSERT INTO `users`(username,password) VALUES (?,?)");
+$register->bind_param("ss",$username,$password_hash);
+$register->execute();
+?>
